30bc8e97aba8626fbcde5c25caddf9c5c7a065b8
Maruja <Maruja@LAPTOP-1NGMOED3>
2022-05-12 18:05:29 -0500

    el 3 del examen

---
+++ src/mesesdelaño/Mesesdelaño.java
@@ -0,0 +1,31 @@
+package mesesdelaño;
+import java.util.ArrayList;
+
+public class Mesesdelaño {
+    
+    ArrayList<String> listMeses = new ArrayList<>();
+
+    public void listMeses(){
+        listMeses.add("Enero");
+        listMeses.add("Febrero");
+        listMeses.add("Marzo");
+        listMeses.add("Abril");
+        listMeses.add("Mayo");
+        listMeses.add("Junio");
+        listMeses.add("Julio");
+        listMeses.add("Agosto");
+        listMeses.add("Septiembre");
+        listMeses.add("Octubre");
+        listMeses.add("Noviembre");
+        listMeses.add("Diciembre");
+        
+        listMeses.forEach((d) -> {
+            System.out.println(""+d);
+        });
+    }
+    
+    public static void main(String[] args) {
+        Mesesdelaño lm = new Mesesdelaño();
+        lm.listMeses();
+    }
+}
